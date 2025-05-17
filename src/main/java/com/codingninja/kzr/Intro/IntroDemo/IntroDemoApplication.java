36ffa56322697ef5a3dc2fa07873fde45641005e
Siddhant Saxena <79580112+siddhant1599@users.noreply.github.com>
2025-05-18 01:08:43 +0530

    1.2 Setting up

---
+++ src/main/java/com/codingninja/kzr/Intro/IntroDemo/IntroDemoApplication.java
@@ -0,0 +1,13 @@
+package com.codingninja.kzr.Intro.IntroDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingninja/kzr/Intro/IntroDemo/IntroDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingninja.kzr.Intro.IntroDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
